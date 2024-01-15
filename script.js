dd33c70afefd4e04ca9dd253c85f640c279c058a
Mohammadreza <hozoorimohammadreza1@gmail.com>
2024-01-15 15:45:01 +0330

    Update HTML, CSS and JS code for the interactive flex component.

---
+++ script.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.querySelector(".container");
+  for (let i = 0; i < 1400; i++) {
+    const box = document.createElement("div");
+    box.classList.add("square_block");
+    container.appendChild(box);
+    box.addEventListener("mouseover", (e) => {
+        box.classList.toggle("active")
+    })
+    box.addEventListener("click", ()=>{
+        box.classList.toggle("active");
+    })
+  }
+  const boxes = document.querySelectorAll(".square_block");
+  setInterval(() => {
+    const randnum = Math.floor(Math.random() * boxes.length);
+    boxes[randnum].classList.toggle("active");
+  }, 10);
+
+
+
+  // toggle menu
+
+  const settings = document.querySelector(".settings")
+  const btn_toggle = document.querySelector(".toggle_setting");
+  const arrows = document.querySelectorAll(".toggle_setting > div");
+  btn_toggle.addEventListener("click", ()=>{
+    arrows.forEach((arrow) => {
+        arrow.classList.toggle("show_menu");
+        arrow.classList.toggle("hide_menu");
+    })
+    settings.classList.toggle("active_settings")
+  })
+
+
+  // change color and size and ... 
+
+  const size = document.querySelector("#size");
+  size.addEventListener("change", (e) => {
+    boxes.forEach((box) => {
+        box.style.height = size.value * 1 + "px"
+        box.style.width = size.value * 1 + "px"
+    })
+  })
+
+
+
+
+
+});
